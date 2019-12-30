6ce753ce4fc4002ab925d98b1794aa17d9fb71b5
Lior Samuel <liordon@gmail.com>
2019-12-30 12:34:39 +0200

    moving from simple best case search to beam search. initial implementation via wordnet

--- test/english/prototype_test.py
+++ test/english/prototype_test.py
@@ -1,70 +1,62 @@
 import pytest
 
-from compilation.definitions import PartOfSpeech, ReservedWord
-from englishPrototype.english_lexer import EnglishLexer, Token
+from englishPrototype.english_lexer import *
 from test_utils.providers import PartOfSpeechVerifier
 
 
 class TestBasicTokenConversionFromNlpToPos(object):
 
     def test_DtIsConvertedToThe(self):
-        assert Token.NlpTagSimplifier["DT"] == ReservedWord.THE.value
+        assert convert_tag_to_part_of_speech("DT") == ReservedWord.THE.value
 
     def test_InIsConvertedToPreposition(self):
-        assert Token.NlpTagSimplifier["IN"] == PartOfSpeech.PREPOSITION.value
+        assert convert_tag_to_part_of_speech("IN") == PartOfSpeech.PREPOSITION.value
 
     def test_JjAndJjsAreConvertedToAdjective(self):
-        assert Token.NlpTagSimplifier["JJ"] == PartOfSpeech.ADJECTIVE.value
-        assert Token.NlpTagSimplifier["JJS"] == PartOfSpeech.ADJECTIVE.value
+        assert convert_tag_to_part_of_speech("JJ") == PartOfSpeech.ADJECTIVE.value
+        assert convert_tag_to_part_of_speech("JJS") == PartOfSpeech.ADJECTIVE.value
 
     def test_NnAndNnsAreConvertedToNoun(self):
-        assert Token.NlpTagSimplifier["NN"] == PartOfSpeech.NOUN.value
-        assert Token.NlpTagSimplifier["NNS"] == PartOfSpeech.NOUN.value
+        assert convert_tag_to_part_of_speech("NN") == PartOfSpeech.NOUN.value
+        assert convert_tag_to_part_of_speech("NNS") == PartOfSpeech.NOUN.value
 
     def test_VbIsConvertedToImperativeVerb(self):
-        assert Token.NlpTagSimplifier["VB"] == PartOfSpeech.V_IMP.value
-
-    def test_VbzIsConvertedToPresentVerb(self):
-        assert Token.NlpTagSimplifier["VBZ"] == PartOfSpeech.V_PRES.value
+        assert convert_tag_to_part_of_speech("VB") == PartOfSpeech.V_IMP.value
 
 
 class EnglishLexerProvided(PartOfSpeechVerifier):
     @pytest.fixture
     def lexer(self):
-        return EnglishLexer()
+        return WordnetProtoLexer()
 
 
 class TestEnglishParserPrototype(EnglishLexerProvided):
 
     def test_canIdentifyCatAsNoun(self, lexer):
         lexer.input("cat")
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
+        self.assertOnePossiblePartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
 
     def test_canIdentifyCatsAsNounAndDisregardPluralism(self, lexer):
         lexer.input("cats")
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
+        self.assertOnePossiblePartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
 
     def test_canIdentifyBigAsAdjective(self, lexer):
         lexer.input("big")
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.ADJECTIVE, lexer)
+        self.assertOnePossiblePartOfSpeechForNextTokenOfLexer(PartOfSpeech.ADJECTIVE, lexer)
 
     def test_canIdentifyCatHatAsTwoConsecutiveNouns(self, lexer):
         lexer.input("cat hat")
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
+        self.assertOnePossiblePartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
+        self.assertOnePossiblePartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
 
     def test_canIdentifyBigManAsConsecutiveAdjectiveAndANoun(self, lexer):
         lexer.input("big man")
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.ADJECTIVE, lexer)
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
-
-    def test_canIdentifyFirstAsANumerator(self, lexer):
-        lexer.input("first")
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.ORDINAL, lexer)
+        self.assertOnePossiblePartOfSpeechForNextTokenOfLexer(PartOfSpeech.ADJECTIVE, lexer)
+        self.assertOnePossiblePartOfSpeechForNextTokenOfLexer(PartOfSpeech.NOUN, lexer)
 
     def test_canIdentifyGoAsAnImperativeVerb(self, lexer):
         lexer.input("go")
-        self.assertPartOfSpeechForNextTokenOfLexer(PartOfSpeech.V_IMP, lexer)
+        self.assertOnePossiblePartOfSpeechForNextTokenOfLexer(PartOfSpeech.V_IMP, lexer)
 
 
 def extract_all_tokens(lexer):
@@ -76,7 +68,6 @@ class TestEnglishLexerPrototypeOnCommands(EnglishLexerProvided):
     def test_turningOnTheLights(self, lexer):
         lexer.input("turn on the lights")
 
-        token_type_list = [t.type for t in (extract_all_tokens(lexer))]
-        # assert PartOfSpeech.V_IMP.value in token_type_list
-        assert ReservedWord.THE.value in token_type_list
-        assert PartOfSpeech.NOUN.value in token_type_list
+        token_type_list = [t.types.keys() for t in (extract_all_tokens(lexer))]
+        assert PartOfSpeech.V_IMP.value in token_type_list[0]
+        assert PartOfSpeech.NOUN.value in token_type_list[-1]
