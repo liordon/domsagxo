9cbb3aa6a7203a6afc78958645679a8737afcae3
Lior Samuel <liordon@gmail.com>
2020-04-16 20:11:07 +0300

    structured node tests into classes

--- test/compilation/node_test.py
+++ test/compilation/node_test.py
@@ -1,136 +1,123 @@
 from compilation.node import *
 
 
-def test_abstractSyntaxTreeNodesCanRepresentThemselvesAndTheirArgs():
-    assert AstNode(1, 2, AstNode()).__repr__() == "AstNode(1, 2, AstNode)"
+class TestNodeRepresentation(object):
+    def test_abstractSyntaxTreeNodesCanRepresentThemselvesAndTheirArgs(self):
+        assert AstNode(1, 2, AstNode()).__repr__() == "AstNode(1, 2, AstNode)"
+
+    def test_abstractSyntaxTreeNodesRepresentationIncludesKwargs(self):
+        assert AstNode(1, 2, shambalulu=3).__repr__() == "AstNode(1, 2, shambalulu=3)"
+
+    def test_basicNodesHaveShorterStringRepresentation(self):
+        assert str(Number(3)) == "3"
+        assert str(String("saluton")) == "`saluton'"
+        assert str(Boolean(True)) == "True"
+        assert str(Boolean(False)) == "False"
+
+    def test_emptyTimeSpansArePrintedAs0seconds(self):
+        assert str(TimeSpan()) == "0s"
+        assert str(TimeSpan(seconds=Number(0))) == "0s"
+
+    def test_compositeTimeSpansAreShownAsNumberAndUnitPairs(self):
+        assert str(TimeSpan(seconds=Number(42))) == "42s"
+        assert str(TimeSpan(minutes=Number(40), seconds=Number(2))) == "40m 2s"
+        assert str(TimeSpan(days=Number(40), hours=Number(2))) == "40d 2h"
+
+    def test_binaryOperationNodesWithAtomicArgumentsCanBeSimplyRepresented(self):
+        assert Add(Number(4), Number(2)).__repr__() == "Add(Number(4), Number(2))"
+        assert Subtract(Number(4), Number(2)).__repr__() == "Subtract(Number(4), Number(2))"
 
 
-def test_abstractSyntaxTreeNodesRepresentationIncludesKwargs():
-    assert AstNode(1, 2, shambalulu=3).__repr__() == "AstNode(1, 2, shambalulu=3)"
+class TestNodePrettyPrinting(object):
+    def test_canUsePrettyPrintFunctionToPrintASingleNode(self):
+        assert AstNode().pretty_print() == "└- AstNode"
 
+    def test_canUsePrettyPrintFunctionOnAtomicNodeTypes(self):
+        assert Number(3).pretty_print() == "└- 3"
+        assert String("saluton").pretty_print() == "└- `saluton'"
+        assert NoneNode().pretty_print() == "└- NoneNode"
 
-def test_basicNodesHaveShorterStringRepresentation():
-    assert str(Number(3)) == "3"
-    assert str(String("saluton")) == "`saluton'"
-    assert str(Boolean(True)) == "True"
-    assert str(Boolean(False)) == "False"
+    def test_canUsePrettyPrintOnVariablesAndArrays(self):
+        assert VariableName('lulo', NoneNode()).pretty_print() == "└- <lulo>"
+        assert VariableName('lulo',
+            Description('mba', Description('sxa', NoneNode()))
+        ).pretty_print() == \
+               "└- <sxa mba lulo>"
 
-
-def test_emptyTimeSpansArePrintedAs0seconds():
-    assert str(TimeSpan()) == "0s"
-    assert str(TimeSpan(seconds=Number(0))) == "0s"
-
-
-def test_compositeTimeSpansAreShownAsNumberAndUnitPairs():
-    assert str(TimeSpan(seconds=Number(42))) == "42s"
-    assert str(TimeSpan(minutes=Number(40), seconds=Number(2))) == "40m 2s"
-    assert str(TimeSpan(days=Number(40), hours=Number(2))) == "40d 2h"
-
-
-def test_binaryOperationNodesWithAtomicArgumentsCanBeSimplyRepresented():
-    assert Add(Number(4), Number(2)).__repr__() == "Add(Number(4), Number(2))"
-    assert Subtract(Number(4), Number(2)).__repr__() == "Subtract(Number(4), Number(2))"
-
-
-def test_canUsePrettyPrintFunctionToPrintASingleNode():
-    assert AstNode().pretty_print() == "└- AstNode"
-
-
-def test_canUsePrettyPrintFunctionOnAtomicNodeTypes():
-    assert Number(3).pretty_print() == "└- 3"
-    assert String("saluton").pretty_print() == "└- `saluton'"
-    assert NoneNode().pretty_print() == "└- NoneNode"
-
-
-def test_canUsePrettyPrintOnVariablesAndArrays():
-    assert VariableName('lulo', NoneNode()).pretty_print() == "└- <lulo>"
-    assert VariableName('lulo',
-        Description('mba', Description('sxa', NoneNode()))
-    ).pretty_print() == \
-           "└- <sxa mba lulo>"
-
-
-def test_canPrettyPrintVariableAssignments():
-    varAss = VariableAssignment(VariableName('lulo', NoneNode()),
-        VariableName('sxambo', NoneNode()))
-    assert varAss.pretty_print() == \
-           "└- VariableAssignment" + \
-           "\n\t├- <lulo>" + \
-           "\n\t└- <sxambo>"
-
-
-def test_canUsePrettyPrintFunctionToPrintSingleDescendantNode():
-    assert AstNode(AstNode()).pretty_print() == \
-           "└- AstNode" + \
-           "\n\t└- AstNode"
-
-
-def test_kwargsAreSpecifiedByNameWhenPrettyPrinted():
-    assert AstNode(AstNode()).pretty_print() == \
-           "└- AstNode" + \
-           "\n\t└- AstNode"
-
-
-def test_canUsePrettyPrintFunctionToPrintTwoDescendantNodes():
-    assert AstNode(AstNode(), AstNode()).pretty_print() == \
-           "└- AstNode" + \
-           "\n\t├- AstNode" + \
-           "\n\t└- AstNode"
-
-
-def test_canUsePrettyPrintFunctionOnDifferentNodeTypes():
-    assert Add(Number(4), Number(2)).pretty_print() == \
-           "└- Add" + \
-           "\n\t├- 4" + \
-           "\n\t└- 2"""
-
-
-def test_routineInvocationsShouldBePrettyPrintedWithTheirArguments():
-    assert RoutineInvocation("sxambalulu", (Number(42),)).pretty_print() == \
-           "└- RoutineInvocation: sxambalulu" + "\n\t└- 42"
-
-
-def test_routineDefinitionsShouldBePrettyPrintedWithTheirStatements():
-    assert RoutineDefinition("sxambalulu", ["kato", "hundo"],
-        VariableAssignment(VariableName('lulo', NoneNode()),
+    def test_canPrettyPrintVariableAssignments(self):
+        varAss = VariableAssignment(VariableName('lulo', NoneNode()),
             VariableName('sxambo', NoneNode()))
-    ).pretty_print() == \
-           "└- RoutineDefinition: sxambalulu" + \
-           "\n\t├- Arguments:" + \
-           "\n\t│\t├- kato" + \
-           "\n\t│\t└- hundo" + \
-           "\n\t└- VariableAssignment" + \
-           "\n\t\t├- <lulo>" + \
-           "\n\t\t└- <sxambo>"
+        assert varAss.pretty_print() == \
+               "└- VariableAssignment" + \
+               "\n\t├- <lulo>" + \
+               "\n\t└- <sxambo>"
 
+    def test_canUsePrettyPrintFunctionToPrintSingleDescendantNode(self):
+        assert AstNode(AstNode()).pretty_print() == \
+               "└- AstNode" + \
+               "\n\t└- AstNode"
 
-def test_canUsePrettyPrintFunctionToPrintManyDescendantNodes():
-    pretty_print = AstNode(
-        AstNode(AstNode(),
-            AstNode(AstNode(), AstNode(), AstNode())
-        )
-    ).pretty_print()
+    def test_kwargsAreSpecifiedByNameWhenPrettyPrinted(self):
+        assert AstNode(AstNode()).pretty_print() == \
+               "└- AstNode" + \
+               "\n\t└- AstNode"
 
-    assert pretty_print == \
-           "└- AstNode" + \
-           "\n\t└- AstNode" + \
-           "\n\t\t├- AstNode" + \
-           "\n\t\t└- AstNode" + \
-           "\n\t\t\t├- AstNode" + \
-           "\n\t\t\t├- AstNode" + \
-           "\n\t\t\t└- AstNode"
+    def test_canUsePrettyPrintFunctionToPrintTwoDescendantNodes(self):
+        assert AstNode(AstNode(), AstNode()).pretty_print() == \
+               "└- AstNode" + \
+               "\n\t├- AstNode" + \
+               "\n\t└- AstNode"
 
+    def test_canUsePrettyPrintFunctionOnDifferentNodeTypes(self):
+        assert Add(Number(4), Number(2)).pretty_print() == \
+               "└- Add" + \
+               "\n\t├- 4" + \
+               "\n\t└- 2"""
 
-def test_whenHavingMultipleChildrenTreeRemainsWellConnected():
-    pretty_print = Add(
-        Multiply(Number(2), Number(3)),
-        Divide(Number(0), Number(5))
-    ).pretty_print()
-    assert pretty_print == \
-           "└- Add" + \
-           "\n\t├- Multiply" + \
-           "\n\t│\t├- 2" + \
-           "\n\t│\t└- 3" + \
-           "\n\t└- Divide" + \
-           "\n\t\t├- 0" + \
-           "\n\t\t└- 5"
+    def test_routineInvocationsShouldBePrettyPrintedWithTheirArguments(self):
+        assert RoutineInvocation("sxambalulu", (Number(42),)).pretty_print() == \
+               "└- RoutineInvocation: sxambalulu" + "\n\t└- 42"
+
+    def test_routineDefinitionsShouldBePrettyPrintedWithTheirStatements(self):
+        assert RoutineDefinition("sxambalulu", ["kato", "hundo"],
+            VariableAssignment(VariableName('lulo', NoneNode()),
+                VariableName('sxambo', NoneNode()))
+        ).pretty_print() == \
+               "└- RoutineDefinition: sxambalulu" + \
+               "\n\t├- Arguments:" + \
+               "\n\t│\t├- kato" + \
+               "\n\t│\t└- hundo" + \
+               "\n\t└- VariableAssignment" + \
+               "\n\t\t├- <lulo>" + \
+               "\n\t\t└- <sxambo>"
+
+    def test_canUsePrettyPrintFunctionToPrintManyDescendantNodes(self):
+        pretty_print = AstNode(
+            AstNode(AstNode(),
+                AstNode(AstNode(), AstNode(), AstNode())
+            )
+        ).pretty_print()
+
+        assert pretty_print == \
+               "└- AstNode" + \
+               "\n\t└- AstNode" + \
+               "\n\t\t├- AstNode" + \
+               "\n\t\t└- AstNode" + \
+               "\n\t\t\t├- AstNode" + \
+               "\n\t\t\t├- AstNode" + \
+               "\n\t\t\t└- AstNode"
+
+    def test_whenHavingMultipleChildrenTreeRemainsWellConnected(self):
+        pretty_print = Add(
+            Multiply(Number(2), Number(3)),
+            Divide(Number(0), Number(5))
+        ).pretty_print()
+        assert pretty_print == \
+               "└- Add" + \
+               "\n\t├- Multiply" + \
+               "\n\t│\t├- 2" + \
+               "\n\t│\t└- 3" + \
+               "\n\t└- Divide" + \
+               "\n\t\t├- 0" + \
+               "\n\t\t└- 5"
+
